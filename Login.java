9f00ec435a57919f8e38dd82cedc30591e116002
Bardia Naderi <bardianaderi@Bardias-MacBook-Pro.local>
2022-10-14 15:21:13 -0400

    added signup

--- Login.java
+++ Login.java
@@ -1,10 +1,9 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Login {
 
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws IOException {
         String filePath = "users.csv";
         try (Scanner input = new Scanner(System.in)) {
             System.out.println("Please enter username");
@@ -13,32 +12,40 @@ public class Login {
             System.out.println("Please enter password");
             String pass = input.nextLine();
 
-            if(verify(user, pass, filePath)) 
+            if (verify(user, pass, filePath)) {
                 System.out.println("You are logged in");
-            else
-                System.out.println("Log in failed");
+            } else {
+                File file = new File(filePath);
+                try (FileWriter printWriter = new FileWriter(file, true)) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append(user);
+                    stringBuilder.append(',');
+                    stringBuilder.append(pass);
+                    stringBuilder.append('\n');
+                    printWriter.write(stringBuilder.toString());
+                }
+                System.out.println("You are a new user, thank you for signing up.");
+            }
         }
     }
 
-    public static boolean verify(String user, String pass, String filePath) throws FileNotFoundException{
+    public static boolean verify(String user, String pass, String filePath) throws FileNotFoundException {
         boolean found = false;
         String tempUser = "";
         String tempPass = "";
         Scanner x = new Scanner(new File(filePath));
         x.useDelimiter("[,\n]");
 
-        while(x.hasNext() && !found){
+        while (x.hasNext() && !found) {
             tempUser = x.next();
             tempPass = x.next();
 
-            if(tempUser.trim().equals(user.trim()) && tempPass.trim().equals(pass.trim())){
+            if (tempUser.trim().equals(user.trim()) && tempPass.trim().equals(pass.trim())) {
                 found = true;
             }
         }
         x.close();
-        return(found);
+        return (found);
     }
-
-
 
 }
