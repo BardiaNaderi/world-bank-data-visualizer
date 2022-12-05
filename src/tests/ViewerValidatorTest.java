be505bee5f3c259fa3f3cc58f74d3e5c537635fc
Bardia Naderi <bardianaderi@Bardias-MacBook-Pro.local>
2022-12-04 22:26:49 -0500

    use case 6

--- src/tests/ViewerValidatorTest.java
+++ src/tests/ViewerValidatorTest.java
@@ -95,4 +95,110 @@ class ViewerValidatorTest {
 		assertTrue(view.getScatter().getDisplay());		
 	}
 	
+	@Test
+	void testTimeChartExistsForValidAnalysis() {
+		ParametersSelector params = new ParametersSelector();
+		params.selectCountry("Canada");
+		params.selectStartYear("2011");
+		params.selectEndYear("2014");
+		params.selectAnalysis("0");
+		params.recalculate();
+		params.addViewer("Time Chart");
+		
+		View view = MainUI.getInstance().getView();
+		
+		assertNotNull(view.getTime().getChart());	
+		
+		assertTrue(view.getTime().getDisplay());		
+	}
+	
+	@Test
+	void testPieChartIsRemovedForValidAnalysis() {
+		ParametersSelector params = new ParametersSelector();
+		params.selectCountry("Canada");
+		params.selectStartYear("2011");
+		params.selectEndYear("2014");
+		params.selectAnalysis("3");
+		params.recalculate();
+		params.addViewer("Pie Chart");
+		params.removeViewer("Pie Chart");
+		
+		View view = MainUI.getInstance().getView();
+		
+		assertNotNull(view.getPie().getChart());
+
+		assertFalse(view.getPie().getDisplay());		
+	}
+	
+	@Test
+	void testBarChartIsRemovedForValidAnalysis() {
+		ParametersSelector params = new ParametersSelector();
+		params.selectCountry("Canada");
+		params.selectStartYear("2011");
+		params.selectEndYear("2014");
+		params.selectAnalysis("0");
+		params.recalculate();
+		params.addViewer("Bar Chart");
+		params.removeViewer("Bar Chart");
+		
+		View view = MainUI.getInstance().getView();
+		
+		assertNotNull(view.getBar().getChart());
+		
+		assertFalse(view.getBar().getDisplay());		
+	}
+	
+	@Test
+	void testLineChartIsRemovedForValidAnalysis() {
+		ParametersSelector params = new ParametersSelector();
+		params.selectCountry("Canada");
+		params.selectStartYear("2011");
+		params.selectEndYear("2014");
+		params.selectAnalysis("0");
+		params.recalculate();
+		params.addViewer("Line Chart");
+		params.removeViewer("Line Chart");
+		
+		View view = MainUI.getInstance().getView();
+		
+		assertNotNull(view.getLine().getChart());		
+		
+		assertFalse(view.getLine().getDisplay());		
+	}
+	
+	@Test
+	void testScatterChartIsRemovedForValidAnalysis() {
+		ParametersSelector params = new ParametersSelector();
+		params.selectCountry("Canada");
+		params.selectStartYear("2011");
+		params.selectEndYear("2014");
+		params.selectAnalysis("0");
+		params.recalculate();
+		params.addViewer("Scatter Chart");
+		params.removeViewer("Scatter Chart");
+		
+		View view = MainUI.getInstance().getView();
+		
+		assertNotNull(view.getScatter().getChart());	
+		
+		assertFalse(view.getScatter().getDisplay());		
+	}
+	
+	@Test
+	void testTimeChartIsRemovedForValidAnalysis() {
+		ParametersSelector params = new ParametersSelector();
+		params.selectCountry("Canada");
+		params.selectStartYear("2011");
+		params.selectEndYear("2014");
+		params.selectAnalysis("0");
+		params.recalculate();
+		params.addViewer("Time Chart");
+		params.removeViewer("Time Chart");
+		
+		View view = MainUI.getInstance().getView();
+		
+		assertNotNull(view.getTime().getChart());	
+		
+		assertFalse(view.getTime().getDisplay());		
+	}
 }
