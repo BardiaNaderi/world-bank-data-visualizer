aead9ce76f7ada6698b6c83cd958b0833d5ad7f6
Bardia Naderi <bardianaderi@Bardias-MacBook-Pro.local>
2022-12-07 20:05:10 -0500

    missed javadocs tests

--- src/tests/ViewerValidatorTest.java
+++ src/tests/ViewerValidatorTest.java
@@ -10,6 +10,11 @@ import userInput.ParametersSelector;
 
 class ViewerValidatorTest {
 
+	/**
+	 * Checking if pie chart exists and can be displayed for the selected analysis that is valid and works for pie charts
+         * 
+	 * @result the pie chart exists - returns not null - and the pie chart can be displayed
+	 */
 	@Test
 	void testPieChartExistsForValidAnalysis() {
 		ParametersSelector params = new ParametersSelector();
@@ -27,6 +32,11 @@ class ViewerValidatorTest {
 		assertTrue(view.getPie().getDisplay());		
 	}
 
+	/**
+	 * Checking if pie chart does not exist and can be displayed for the selected analysis that is valid but does not work for pie charts
+         * 
+	 * @result the pie chart does not exist - returns null - and the pie chart can be displayed
+	 */
 	@Test
 	void testPieChartDoesNotExistForInvalidAnalysis() {
 		ParametersSelector params = new ParametersSelector();
@@ -44,6 +54,11 @@ class ViewerValidatorTest {
 		assertTrue(view.getPie().getDisplay());		
 	}
 	
+	/**
+	 * Checking if bar chart exists and can be displayed for the selected analysis that is valid
+         * 
+	 * @result the bar chart exists - returns not null - and the bar chart can be displayed
+	 */
 	@Test
 	void testBarChartExistsForValidAnalysis() {
 		ParametersSelector params = new ParametersSelector();
@@ -61,6 +76,11 @@ class ViewerValidatorTest {
 		assertTrue(view.getBar().getDisplay());		
 	}
 	
+	/**
+	 * Checking if line chart exists and can be displayed for the selected analysis that is valid
+         * 
+	 * @result the line chart exists - returns not null - and the line chart can be displayed
+	 */
 	@Test
 	void testLineChartExistsForValidAnalysis() {
 		ParametersSelector params = new ParametersSelector();
@@ -78,6 +98,11 @@ class ViewerValidatorTest {
 		assertTrue(view.getLine().getDisplay());		
 	}
 	
+	/**
+	 * Checking if scatter chart exists and can be displayed for the selected analysis that is valid
+         * 
+	 * @result the scatter chart exists - returns not null - and the scatter chart can be displayed
+	 */
 	@Test
 	void testScatterChartExistsForValidAnalysis() {
 		ParametersSelector params = new ParametersSelector();
@@ -95,6 +120,11 @@ class ViewerValidatorTest {
 		assertTrue(view.getScatter().getDisplay());		
 	}
 	
+	/**
+	 * Checking if time chart exists and can be displayed for the selected analysis that is valid
+         * 
+	 * @result the time chart exists - returns not null - and the time chart can be displayed
+	 */
 	@Test
 	void testTimeChartExistsForValidAnalysis() {
 		ParametersSelector params = new ParametersSelector();
@@ -112,6 +142,11 @@ class ViewerValidatorTest {
 		assertTrue(view.getTime().getDisplay());		
 	}
 	
+	/**
+	 * Checking if pie chart is removed and is not being displayed for valid analysis
+         * 
+	 * @result the pie chart is removed but exists - returns not null - and the pie chart is not displayed
+	 */
 	@Test
 	void testPieChartIsRemovedForValidAnalysis() {
 		ParametersSelector params = new ParametersSelector();
@@ -130,6 +165,11 @@ class ViewerValidatorTest {
 		assertFalse(view.getPie().getDisplay());		
 	}
 	
+	/**
+	 * Checking if bar chart is removed and is not being displayed for valid analysis
+         * 
+	 * @result the bar chart is removed but exists - returns not null - and the bar chart is not displayed
+	 */
 	@Test
 	void testBarChartIsRemovedForValidAnalysis() {
 		ParametersSelector params = new ParametersSelector();
@@ -148,6 +188,11 @@ class ViewerValidatorTest {
 		assertFalse(view.getBar().getDisplay());		
 	}
 	
+	/**
+	 * Checking if line chart is removed and is not being displayed for valid analysis
+         * 
+	 * @result the line chart is removed but exists - returns not null - and the line chart is not displayed
+	 */
 	@Test
 	void testLineChartIsRemovedForValidAnalysis() {
 		ParametersSelector params = new ParametersSelector();
@@ -166,6 +211,11 @@ class ViewerValidatorTest {
 		assertFalse(view.getLine().getDisplay());		
 	}
 	
+	/**
+	 * Checking if scatter chart is removed and is not being displayed for valid analysis
+         * 
+	 * @result the scatter chart is removed but exists - returns not null - and the scatter chart is not displayed
+	 */
 	@Test
 	void testScatterChartIsRemovedForValidAnalysis() {
 		ParametersSelector params = new ParametersSelector();
@@ -184,6 +234,11 @@ class ViewerValidatorTest {
 		assertFalse(view.getScatter().getDisplay());		
 	}
 	
+	/**
+	 * Checking if time chart is removed and is not being displayed for valid analysis
+         * 
+	 * @result the time chart is removed but exists - returns not null - and the time chart is not displayed
+	 */
 	@Test
 	void testTimeChartIsRemovedForValidAnalysis() {
 		ParametersSelector params = new ParametersSelector();
